9eee8d61c9cb484f89bd33381340b09fed0f902f
Eugene <Kivooeo123@gmail.com>
2021-10-29 20:36:16 +0500

    First Commit

---
+++ pi.py
@@ -0,0 +1,17 @@
+def ddos():
+        a = 0
+        while True:
+            
+            a = a + 1
+            print(f'error {a}')
+def putin():
+    print('youre fucking right man, come one')
+    input('end?')
+def aboba():
+    a = int(input('~$ '))
+    if a == 98:
+        ddos()
+    else:
+        putin()
+
+aboba()
